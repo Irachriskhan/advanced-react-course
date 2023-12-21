23bdd6d3672a1c28af58f670ca9a9187c54dc5ae
christophe Irakoze <irachriskhan@gmail.com>
2023-12-21 19:38:53 +0200

    setting up multiple returns

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Setup from "./chapters/2-useEffect/3-useEffect-fetch-data";
+import Setup from "./chapters/3-conditional-rendering/1-multiple-returns";
 
 function App() {
   return (
     <div className="container">
-      <h1>React JS- Advanced Tutorial</h1>
+      <h1>Conditional Rendering</h1>
       <Setup />
     </div>
   );
