cba69626a932c3cc3b261e8e253d088761e80fff
christophe Irakoze <irachriskhan@gmail.com>
2023-12-26 13:21:01 +0200

    add controlled input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Setup from "./chapters/3-conditional-rendering/3-show-hide";
+import Setup from "./chapters/4-forms/1-controlled-inputs";
 
 function App() {
   return (
     <div className="container">
-      <h1>Conditional Rendering</h1>
+      <h1>React forms</h1>
       <Setup />
     </div>
   );
