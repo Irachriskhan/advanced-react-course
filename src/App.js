51378614e9b72091c94a624be5b4a86b04cc3732
Christophe Irakoze <irachriskhan@gmail.com>
2024-01-05 17:05:04 +0200

    start working on useContext

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import Setup from "./chapters/7-prop-drilling/1-prop-drilling";
+import Setup from "./chapters/8-useContext/1-context-api";
 
 function App() {
   return (
